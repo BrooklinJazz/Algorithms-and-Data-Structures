efac1df78cd26a4d4f2f7e39b4dda4b392169f49
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-10-24 19:21:14 -0700

    algorithm saturday

--- algorithmic_design_and_techniques/greedy_algorithms.py
+++ algorithmic_design_and_techniques/greedy_algorithms.py
@@ -30,3 +30,156 @@ def maximum_loot(data):
         remainingCapacity -= allowed_weight
         result += item["ratio"] * allowed_weight
     return result
+
+
+def maximum_advertising_revenue(slots, profit_per_click, clicks_per_day):
+    profit_per_click.sort()
+    clicks_per_day.sort()
+    result = 0
+    for profit, clicks in zip(profit_per_click, clicks_per_day):
+        result += profit * clicks
+    return result
+
+
+def low_high_values(segments):
+    lowest, highest = segments[0][0], segments[0][1]
+    for s in segments:
+        lowest = min(lowest, s[0])
+        highest = max(highest, s[1])
+    return (lowest, highest)
+
+
+def sort_segments(segments):
+    clone = segments
+
+    def lowest_start(segment):
+        return segment[0]
+    clone.sort(key=lowest_start)
+    return clone
+
+
+# def collect_signatures(segments):
+#     lowest, highest = low_high_values(segments)
+#     sorted_segments = sort_segments(segments)
+#     marks = []
+#     for i in range(highest - lowest):
+#         n = lowest + i
+
+
+def is_overlapping(mark, segment):
+    return mark >= segment[0] and mark <= segment[1]
+
+
+def number_of_overlapping(mark, segments):
+    result = 0
+    for s in segments:
+        if is_overlapping(mark, s):
+            result += 1
+    return result
+
+# def number_of_new_overlapping(mark, marks, segments):
+#     def by_is_handled():
+
+#     filtered_segments = filter()
+
+
+def is_segment_handled(marks, s):
+    result = False
+    for m in marks:
+        if s[0] <= m and s[1] >= m:
+            result = True
+    return result
+
+
+# def collect_signatures(unsorted_segments):
+#     marks = []
+#     segments = sort_segments(unsorted_segments)
+#     for s in segments:
+#         best_mark = 0
+#         for i in range((s[1] + 1) - s[0]):
+#             m = i + s[0]
+#             overlapping = number_of_overlapping(m, segments)
+#             if overlapping >= best_mark:
+#                 best_mark = m
+#         if best_mark not in marks and not is_segment_handled(marks, s):
+#             marks.append(best_mark)
+
+#     return marks
+
+
+# def has_disjointed(marks, segments):
+#     result = False
+#     smallest = min(marks)
+#     biggest = max(marks)
+#     for s in segments:
+#         if s[0] > biggest or s[1] < smallest:
+#             result = True
+#     return result
+
+def has_disjointed(marks, segments):
+    disjointed = False
+
+    def no_marks_between_segment(segment):
+        result = True
+        for m in marks:
+            if segment[0] <= m and segment[1] >= m:
+                result = False
+        return result
+
+    for s in segments:
+        if no_marks_between_segment(s):
+            disjointed = True
+    return disjointed
+
+
+def get_smallest_right_value(segments):
+    result = segments[0][1]
+    for s in segments:
+        result = min(result, s[2])
+    return result
+
+
+def collect_signatures(segments):
+    marks = []
+    while segments != []:
+        smallest_right = segments[0][1]
+        for s in segments:
+            smallest_right = min(smallest_right, s[1])
+        marks.append(smallest_right)
+
+        def right_is_above_smallest(s):
+            return s[0] > smallest_right
+
+        segments = list(filter(right_is_above_smallest, segments))
+    return marks
+
+
+def collect_signatures_broken(segments):
+    low, high = low_high_values(segments)
+    all_marks = list(range(low, high+1))
+    marks = list(range(low, high+1))
+    for m in all_marks:
+        marks.remove(m)
+        if has_disjointed(marks, segments):
+            marks.append(m)
+    return marks
+
+
+def collect_signatures_from_string(str):
+    n, *data = map(int, str.split())
+    segments = list(map(lambda x: [x[0], x[1]], zip(data[::2], data[1::2])))
+    return collect_signatures(segments)
+
+
+def optimal_summands(n):
+    arr = []
+    i = 0
+    while True:
+        i += 1
+        if n - i <= i:
+            arr.append(n)
+            break
+        else:
+            arr.append(i)
+            n -= i
+    return arr
--- algorithmic_design_and_techniques/greedy_algothims_test.py
+++ algorithmic_design_and_techniques/greedy_algothims_test.py
@@ -1,6 +1,6 @@
 import unittest
 import random
-from greedy_algorithms import money_change, maximum_loot
+from greedy_algorithms import optimal_summands, collect_signatures_from_string, has_disjointed, money_change, maximum_loot, maximum_advertising_revenue, collect_signatures, number_of_overlapping
 
 
 class MoneyChange(unittest.TestCase):
@@ -13,10 +13,71 @@ class MoneyChange(unittest.TestCase):
 
 class MaximumLoot(unittest.TestCase):
 
-    def test_simple_cases(self):
+    def test_simple_case(self):
         result = maximum_loot(
             [3, 50, 60, 20, 100, 50, 120, 30])
         self.assertEquals(result, 180.0000)
+
+
+class MaxAdvertisingRevenue(unittest.TestCase):
+    def test_array_length_1(self):
+        slots = 1
+        profit_per_click = [23]
+        clicks_per_day = [39]
+        result = maximum_advertising_revenue(
+            slots, profit_per_click, clicks_per_day)
+        self.assertEquals(result, 897)
+
+    def test_array_length_3(self):
+        slots = 3
+        profit_per_click = [1, 3, -5]
+        clicks_per_day = [-2, 4, 1]
+        result = maximum_advertising_revenue(
+            slots, profit_per_click, clicks_per_day)
+        self.assertEquals(result, 23)
+
+
+class CollectingSignatures(unittest.TestCase):
+    def test_list_with_single_result(self):
+        segments = [[1, 3], [2, 5], [3, 6]]
+        self.assertEqual(collect_signatures(segments), [3])
+
+    def test_list_with_two_results(self):
+        segments = [[4, 7], [1, 3], [2, 5], [5, 6]]
+        self.assertEquals(collect_signatures(segments), [3, 6])
+
+    def test_single_segment(self):
+        self.assertEquals(collect_signatures([[4, 7]]), [7])
+
+    def test_single_segment_same_values(self):
+        self.assertEquals(collect_signatures([[3, 3]]), [3])
+
+    def test_large_data(self):
+        large_data = []
+        for i in range(100):
+            large_data.append([random.randint(0, 100), 10 ** 9])
+        self.assertEquals(collect_signatures(large_data), [10 ** 9])
+
+    def test_string_input(self):
+        self.assertEquals(collect_signatures_from_string(
+            "3 1 3 2 5 3 6"), [3])
+
+    def test_has_disjointed(self):
+        segments = [[4, 7], [1, 3], [2, 5], [5, 6]]
+        self.assertTrue(has_disjointed([2], segments))
+        self.assertTrue(has_disjointed([7, 3], segments))
+        self.assertFalse(has_disjointed([3, 6], segments))
+
+
+class MaximumNumberOfPrizes(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEquals(optimal_summands(6), [1, 2, 3])
+
+    def test_case_2(self):
+        self.assertEquals(optimal_summands(8), [1, 2, 5])
+
+    def test_case_3(self):
+        self.assertEquals(optimal_summands(2), [2])
 
 
 if __name__ == '__main__':
