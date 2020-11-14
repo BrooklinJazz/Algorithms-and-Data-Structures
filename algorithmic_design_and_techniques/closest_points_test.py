56f844f53121e1de6c01e2e1c12933978701ab99
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-11-14 15:16:02 -0700

    edit distance

--- algorithmic_design_and_techniques/closest_points_test.py
+++ algorithmic_design_and_techniques/closest_points_test.py
@@ -86,119 +86,119 @@ def divide_chunks(l, n):
 tested_closest_points = closest_points
 
 
-class ClosestPoints(unittest.TestCase):
-    def test_sort_x(self):
-        self.assertEqual(sortX([[1, 0], [0, 2]]), [[0, 2], [1, 0]])
+# class ClosestPoints(unittest.TestCase):
+#     def test_sort_x(self):
+#         self.assertEqual(sortX([[1, 0], [0, 2]]), [[0, 2], [1, 0]])
 
-    def test_sort_y(self):
-        self.assertEqual(sortY([[0, 2], [1, 0]]), [[1, 0], [0, 2]])
+#     def test_sort_y(self):
+#         self.assertEqual(sortY([[0, 2], [1, 0]]), [[1, 0], [0, 2]])
 
-    def test_elapsed_time(self):
-        num = 10 ** 5
-        points = list(range(num * 2))
-        random.shuffle(points)
-        start = time.monotonic()
-        points = fast_format(points, len(points))
-        tested_closest_points(points, len(points))
-        end = time.monotonic()
-        elapsed = end - start
-        self.assertTrue(elapsed < 5)
+#     def test_elapsed_time(self):
+#         num = 10 ** 5
+#         points = list(range(num * 2))
+#         random.shuffle(points)
+#         start = time.monotonic()
+#         points = fast_format(points, len(points))
+#         tested_closest_points(points, len(points))
+#         end = time.monotonic()
+#         elapsed = end - start
+#         self.assertTrue(elapsed < 5)
 
-    def test_simple(self):
-        points = [[0, 0], [3, 4]]
-        self.assertEqual(tested_closest_points(points, len(points)), 5)
+#     def test_simple(self):
+#         points = [[0, 0], [3, 4]]
+#         self.assertEqual(tested_closest_points(points, len(points)), 5)
 
-    def test_against_naive(self):
-        num = 100
-        points = list(range(num * 2))
-        random.shuffle(points)
-        points = fast_format(points, len(points))
-        self.assertEqual(tested_closest_points(points, len(points)),
-                         brute_force(points, len(points)))
+#     def test_against_naive(self):
+#         num = 100
+#         points = list(range(num * 2))
+#         random.shuffle(points)
+#         points = fast_format(points, len(points))
+#         self.assertEqual(tested_closest_points(points, len(points)),
+#                          brute_force(points, len(points)))
 
-    def test_naive_coinciding(self):
-        points = [[7, 7], [1, 100], [4, 8], [7, 7]]
-        self.assertEqual(brute_force(points, len(points)), 0)
+#     def test_naive_coinciding(self):
+#         points = [[7, 7], [1, 100], [4, 8], [7, 7]]
+#         self.assertEqual(brute_force(points, len(points)), 0)
 
-    def test_naive(self):
-        points = [[0, 0], [3, 4]]
-        self.assertEqual(brute_force(points, len(points)), 5)
+#     def test_naive(self):
+#         points = [[0, 0], [3, 4]]
+#         self.assertEqual(brute_force(points, len(points)), 5)
 
-    def test_fast_format(self):
-        self.assertEqual(fast_format([0, 1, 2, 3], 4), [[0, 1], [2, 3]])
-
-
-def brute(ax):
-    mi = dist(ax[0], ax[1])
-    p1 = ax[0]
-    p2 = ax[1]
-    ln_ax = len(ax)
-    if ln_ax == 2:
-        return p1, p2, mi
-    for i in range(ln_ax-1):
-        for j in range(i + 1, ln_ax):
-            if i != 0 and j != 1:
-                d = dist(ax[i], ax[j])
-                if d < mi:  # Update min_dist and points
-                    mi = d
-                    p1, p2 = ax[i], ax[j]
-    return p1, p2, mi
+#     def test_fast_format(self):
+#         self.assertEqual(fast_format([0, 1, 2, 3], 4), [[0, 1], [2, 3]])
 
 
-def closest_split_pair(p_x, p_y, delta, best_pair):
-    ln_x = len(p_x)  # store length - quicker
-    mx_x = p_x[ln_x // 2][0]  # select midpoint on x-sorted array
-    # Create a subarray of points not further than delta from
-    # midpoint on x-sorted array
-    s_y = [x for x in p_y if mx_x - delta <= x[0] <= mx_x + delta]
-    best = delta  # assign best value to delta
-    ln_y = len(s_y)  # store length of subarray for quickness
-    for i in range(ln_y - 1):
-        for j in range(i+1, min(i + 7, ln_y)):
-            p, q = s_y[i], s_y[j]
-            dst = dist(p, q)
-            if dst < best:
-                best_pair = p, q
-                best = dst
-    return best_pair[0], best_pair[1], best
+# def brute(ax):
+#     mi = dist(ax[0], ax[1])
+#     p1 = ax[0]
+#     p2 = ax[1]
+#     ln_ax = len(ax)
+#     if ln_ax == 2:
+#         return p1, p2, mi
+#     for i in range(ln_ax-1):
+#         for j in range(i + 1, ln_ax):
+#             if i != 0 and j != 1:
+#                 d = dist(ax[i], ax[j])
+#                 if d < mi:  # Update min_dist and points
+#                     mi = d
+#                     p1, p2 = ax[i], ax[j]
+#     return p1, p2, mi
 
-def solution(x, y):
-    a = list(zip(x, y))  # This produces list of tuples
-    ax = sorted(a, key=lambda x: x[0])  # Presorting x-wise
-    ay = sorted(a, key=lambda x: x[1])  # Presorting y-wise
-    p1, p2, mi = closest_pair(ax, ay)  # Recursive D&C function
-    return mi
 
-def closest_pair(ax, ay):
-    ln_ax = len(ax)  # It's quicker to assign variable
-    if ln_ax <= 3:
-        return brute(ax)  # A call to bruteforce comparison
-    mid = ln_ax // 2  # Division without remainder, need int
-    Qx = ax[:mid]  # Two-part split
-    Rx = ax[mid:]
-    # Determine midpoint on x-axis
-    midpoint = ax[mid][0]
-    Qy = list()
-    Ry = list()
-    for x in ay:  # split ay into 2 arrays using midpoint
-        if x[0] <= midpoint:
-            Qy.append(x)
-        else:
-            Ry.append(x)
-    # Call recursively both arrays after split
-    (p1, q1, mi1) = closest_pair(Qx, Qy)
-    (p2, q2, mi2) = closest_pair(Rx, Ry)
-    # Determine smaller distance between points of 2 arrays
-    if mi1 <= mi2:
-        d = mi1
-        mn = (p1, q1)
-    else:
-        d = mi2
-        mn = (p2, q2)
-    # Call function to account for points on the boundary
-    (p3, q3, mi3) = closest_split_pair(ax, ay, d, mn)
-    # Determine smallest distance for the array
-    if d <= mi3:
-        return mn[0], mn[1], d
-    else:
-        return p3, q3, mi3
+# def closest_split_pair(p_x, p_y, delta, best_pair):
+#     ln_x = len(p_x)  # store length - quicker
+#     mx_x = p_x[ln_x // 2][0]  # select midpoint on x-sorted array
+#     # Create a subarray of points not further than delta from
+#     # midpoint on x-sorted array
+#     s_y = [x for x in p_y if mx_x - delta <= x[0] <= mx_x + delta]
+#     best = delta  # assign best value to delta
+#     ln_y = len(s_y)  # store length of subarray for quickness
+#     for i in range(ln_y - 1):
+#         for j in range(i+1, min(i + 7, ln_y)):
+#             p, q = s_y[i], s_y[j]
+#             dst = dist(p, q)
+#             if dst < best:
+#                 best_pair = p, q
+#                 best = dst
+#     return best_pair[0], best_pair[1], best
+
+# def solution(x, y):
+#     a = list(zip(x, y))  # This produces list of tuples
+#     ax = sorted(a, key=lambda x: x[0])  # Presorting x-wise
+#     ay = sorted(a, key=lambda x: x[1])  # Presorting y-wise
+#     p1, p2, mi = closest_pair(ax, ay)  # Recursive D&C function
+#     return mi
+
+# def closest_pair(ax, ay):
+#     ln_ax = len(ax)  # It's quicker to assign variable
+#     if ln_ax <= 3:
+#         return brute(ax)  # A call to bruteforce comparison
+#     mid = ln_ax // 2  # Division without remainder, need int
+#     Qx = ax[:mid]  # Two-part split
+#     Rx = ax[mid:]
+#     # Determine midpoint on x-axis
+#     midpoint = ax[mid][0]
+#     Qy = list()
+#     Ry = list()
+#     for x in ay:  # split ay into 2 arrays using midpoint
+#         if x[0] <= midpoint:
+#             Qy.append(x)
+#         else:
+#             Ry.append(x)
+#     # Call recursively both arrays after split
+#     (p1, q1, mi1) = closest_pair(Qx, Qy)
+#     (p2, q2, mi2) = closest_pair(Rx, Ry)
+#     # Determine smaller distance between points of 2 arrays
+#     if mi1 <= mi2:
+#         d = mi1
+#         mn = (p1, q1)
+#     else:
+#         d = mi2
+#         mn = (p2, q2)
+#     # Call function to account for points on the boundary
+#     (p3, q3, mi3) = closest_split_pair(ax, ay, d, mn)
+#     # Determine smallest distance for the array
+#     if d <= mi3:
+#         return mn[0], mn[1], d
+#     else:
+#         return p3, q3, mi3
--- algorithmic_design_and_techniques/edit_distance_test.py
+++ algorithmic_design_and_techniques/edit_distance_test.py
@@ -2,64 +2,49 @@ import unittest
 import sys
 
 
-def insert(a, b):
-    pass
-
-
 operations = []
 
 
 def edit_distance(a_str, b_str):
-    row_a, row_b = create_sequence_matrix(a_str, b_str)
-    return alignment_cost(row_a, row_b)
-
-def create_sequence_matrix
-
-# def create_sequence_matrix(a_str, b_str):
-#     a_row = list(a_str)
-#     b_row = list(b_str)
-#     a_crawler, b_crawler = 0, 0
-#     i = 0
-#     while a_crawler < len(a_str) and b_crawler < len(b_str):
-#         deletion_cost, insertion_cost, current_cost = sys.maxsize, sys.maxsize, alignment_cost(
-#             a_row, b_row)
-#         if a_str[a_crawler] == b_str[b_crawler]:
-#             b_crawler += 1
-#             a_crawler += 1
-#         else:
-#             temp_b_row = b_row.copy()
-#             temp_b_row.insert(i, " ")
-#             deletion_cost = alignment_cost(a_row, temp_b_row)
-
-#             temp_a_row = a_row.copy()
-#             temp_a_row.insert(i, " ")
-#             insertion_cost = alignment_cost(temp_a_row, b_row)
-#             if current_cost == min(deletion_cost, insertion_cost, current_cost):
-#                 b_crawler += 1
-#                 a_crawler += 1
-#             elif deletion_cost < insertion_cost:
-#                 b_row = temp_b_row
-#                 b_crawler += 1
-
-#             elif insertion_cost < deletion_cost:
-#                 a_row = temp_a_row
-#                 a_crawler += 1
-#             else:
-#                 print("why am I here?")
-#                 b_crawler += 1
-#                 a_crawler += 1
-#             i += 1 # should this also be on if?
-#     while len(a_row) < len(b_row):
-#         a_row.append(" ")
-#     return a_row, b_row
+    a_len = len(a_str)
+    b_len = len(b_str)
+    dp = [[0 for b in range(b_len + 1)] for a in range(a_len + 1)]
+    if a_len == 0 or b_len == 0:
+        return max(b_len, a_len)
+    for a_index in range(a_len + 1):
+        for b_index in range(b_len + 1):
+            b_char = b_str[b_index - 1]
+            a_char = a_str[a_index - 1]
+            if a_index == 0:
+                dp[a_index][b_index] = b_index
+            elif b_index == 0:
+                dp[a_index][b_index] = a_index
+            elif a_char == b_char:
+                dp[a_index][b_index] = dp[a_index - 1][b_index - 1]
+            else:
+                # [b1a1, b1a2]
+                # [b2a1, b2a2]
+                b1a1 = dp[a_index - 1][b_index - 1]
+                b1a2 = dp[a_index][b_index - 1]
+                b2a1 = dp[a_index - 1][b_index]
+                dp[a_index][b_index] = 1 + min(b1a1, b1a2, b2a1)
+    return dp[a_len][b_len]
 
 
-def alignment_cost(a_row, b_row):
+def create_sequence_matrix(a_str, b_str):
+    pass
+
+
+def score(a_row, b_row):
     score = 0
     for a, b in zip(a_row, b_row):
         if a == b:
             score += 1
-    return max(len(a_row), len(b_row)) - score
+    return score
+
+
+def alignment_cost(a_row, b_row):
+    return max(len(a_row), len(b_row)) - score(a_row, b_row)
 
 
 class EditDistance(unittest.TestCase):
@@ -70,70 +55,36 @@ class EditDistance(unittest.TestCase):
     def test_short_ports(self):
         self.assertEqual(edit_distance("short", "ports"), 3)
 
-    def test_deletion(self):
-        self.assertEqual(edit_distance("ed", "d"), 1)
+    def test_0_char_str(self):
+        self.assertEqual(edit_distance("e", ""), 1)
 
-    def test_insertion(self):
-        self.assertEqual(edit_distance("db", "ds"), 1)
+    def test_1_and_7_char_str(self):
+        self.assertEqual(edit_distance("e", "editing"), 6)
 
-    def test_editing_distance(self):
+    def test_1_char_str(self):
+        self.assertEqual(edit_distance("e", "d"), 1)
+
+    def test_2_char_str(self):
+        self.assertEqual(edit_distance("ed", "di"), 2)
+
+    def test_3_char_str(self):
+        self.assertEqual(edit_distance("edi", "dis"), 2)
+
+    def test_4_char_str(self):
+        self.assertEqual(edit_distance("edit", "dist"), 2)
+
+    def test_5_char_str(self):
+        self.assertEqual(edit_distance("editi", "dista"), 3)
+
+    def test_6_char_str(self):
+        self.assertEqual(edit_distance("editin", "distan"), 3)
+
+    def test_7_char_str(self):
+        self.assertEqual(edit_distance("editing", "distanc"), 4)
+
+    def test_8_char_str(self):
         self.assertEqual(edit_distance("editing", "distance"), 5)
 
-    # Sequence Matrix
-    def test_sequence_matrix_editing_distance(self):
-        self.assertEqual(create_sequence_matrix("editing", "distance"),
-                         (
-            ["e", "d", "i", " ", "t", "i", "n", "g", " "],
-            [" ", "d", "i", "s", "t", "a", "n", "c", "e"]
-        )
-        )
-
-    def test_sequence_matrix_short_ports(self):
-        self.assertEqual(create_sequence_matrix("short", "ports"),
-                         (
-            ["s", "h", "o", "r", "t", " "],
-            [" ", "p", "o", "r", "t", "s"]
-        )
-        )
-
-    def test_sequence_matrix_matching(self):
-        self.assertEqual(create_sequence_matrix(
-            "ed", "ed"), (["e", "d"], ["e", "d"]))
-
-    def test_sequence_matrix_deletion(self):
-        self.assertEqual(create_sequence_matrix("ed", "d"),
-                         (["e", "d"],
-                          [" ", "d"])
-                         )
-
-    def test_sequence_matrix_non_matching(self):
-        self.assertEqual(create_sequence_matrix("ds", "da"),
-                         (["d", "s"],
-                          ["d", "a"])
-                         )
-
-    def test_sequence_matrix_insertion(self):
-        self.assertEqual(create_sequence_matrix("d", "ed"),
-                         ([" ", "d"],
-                          ["e", "d"])
-                         )
-
-    # Alignment Cost
-    def test_alignment_cost_all_matching(self):
-        self.assertEqual(alignment_cost(["A", "B"],
-                                        ["A", "B"]), 0)
-
-    def test_alignment_cost_diff_size(self):
-        self.assertEqual(alignment_cost(["A"],
-                                        ["A", "B"]), 1)
-
-    def test_alignment_cost_with_mismatch(self):
-        self.assertEqual(alignment_cost(["e", "d", "i", " ", "t", "i", "n", "g", " "],
-                                        [" ", "d", "i", "s", "t", "a", "n", "c", "e"]), 5)
-
-    def test_alignment_cost_short_ports(self):
-        self.assertEqual(
-            alignment_cost(
-                ["S", "H", "O", "R", "T", " "],
-                [" ", "P", "O", "R", "T", "S"]
-            ), 3)
+    def test_failing(self):
+        self.assertEqual(edit_distance(
+            "zoologicoarchaeologist", "zoogeologist"), 10)
