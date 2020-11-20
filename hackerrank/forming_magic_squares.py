4816396f7b2408339d8258cdd8ded60271c591d3
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-11-20 08:25:40 -0700

    test

--- hackerrank/forming_magic_squares.py
+++ hackerrank/forming_magic_squares.py
@@ -2,11 +2,9 @@ import unittest
 
 method_under_test = formingMagicSquare
 
+
 class FormMagicSquares(unittest.TestCase):
     def test_1_change(self):
-        s = [
-            [4, 9, 1],
-            [3, 5, 7],
-            [8, 1, 5]
-        ]
+        s = [[4, 9, 1], [3, 5, 7], [8, 1, 5]]
         self.assertEqual(formingMagicSquare(s))
+        # test
